073f4e3cae3629328f181bcfdb9d45bb18677f2c
Levi Pole <leviwp48@gmail.com>
2025-08-28 19:14:23 -0700

    adding raycasting for bullets

---
+++ godot_games/3d/Val v1/scripts/bullet.gd
@@ -0,0 +1,20 @@
+extends RayCast3D
+
+@export var speed := 4.0
+@export var range := 40.0
+
+var traveled_distance = 0.0
+
+func _physics_process(delta: float) -> void:
+	position += global_basis * Vector3.FORWARD * speed * delta
+	target_position += Vector3.FORWARD * speed * delta
+	force_raycast_update()
+	var collider = get_collider()
+	if is_colliding():
+		global_position = get_collision_point()
+		set_physics_process(false)
+	#traveled_distance += SPEED * delta
+	#if traveled_distance > RANGE:
+		   
+func cleanup() -> void:
+	queue_free()
